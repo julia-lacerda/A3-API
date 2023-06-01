ee29284c0e9b22b4b2348894eb82e391e453f2c6
julia-lacerda <juliaoclacerda@gmail.com>
2023-06-01 02:56:01 -0300

    (feat): add caronexApplication

---
+++ src/main/java/com/uam/caronex/CaronexApplication.java
@@ -0,0 +1,13 @@
+package com.uam.caronex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronexApplication.class, args);
+	}
+
+}
